768c65e149a399a5e1d4d1b59e96fcbc118f904f
Gilson Junio Sales Andrade <gilsonjunio@quimicojapones.com.br>
2024-07-11 21:36:52 -0300

    VERIFICACAO DE EMAILS CONCLUIDA E FUNCIONAL

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/*TODO
+SISTEMA DE SALVAR USUARIOS
+
+
+
+
+
+
+
+*/
+/*
+O SISTEMA DE SALVAR USUARIOS SÓ CONSEGUE SALVAR UM UNICO USUARIO 11/05/2024
+
+*/
+
 async function carregar_Dados(){
 	let buscar = await fetch('https://raw.githubusercontent.com/GilsonJunio/Alunos-Do-Lets-Code-2024/main/data.json');
 	console.log(buscar);
@@ -17,7 +32,7 @@ carregar_Dados()
 let enviar = document.getElementById('enviar');
 enviar.addEventListener('click',() =>{
 
-//SALVAR NOVOS USUÁRIOS	
+	//SALVAR NOVOS USUÁRIOS	
 	let email = document.getElementById('email').value;
 		console.log(email);
 	
@@ -35,10 +50,24 @@ enviar.addEventListener('click',() =>{
 	let usuarios_novos = [];
 		usuarios_novos.push(dados_coletados)
 		console.log(usuarios_novos)
+	
+	localStorage.setItem('usuarios novos',JSON.stringify( usuarios_novos))
+	
+	// VERIFICAR SE EMAIL JÁ EXISTE
 
-	let salvar_usuarioNovos = localStorage.setItem('usuarios novos',JSON.stringify( usuarios_novos))
-		console.log(salvar_usuarioNovos)
-//
+	let buscar_usuarios = JSON.parse(localStorage.getItem('usuarios'))
+		console.log(buscar_usuarios)
+		console.log(buscar_usuarios.email)
+
+	let procurar_email = buscar_usuarios.find((buscar_usuarios) => buscar_usuarios.email === email) || false;
+		console.log(procurar_email)
+
+	if(procurar_email === false){
+		alert("ESTE EMAIL NÃO FOI CADASTRADO")
+	}
+	else{
+		alert("ESTE EMAIL JÁ FOI CADASTRADO")
+	}
 
 //	let dados_comparar = dados_coletados.find((usuarios_novos) => dados_coletados.email_repassado === email && dados_coletados.senha_repassada === senha)
 //		console.log(dados_comparar)
