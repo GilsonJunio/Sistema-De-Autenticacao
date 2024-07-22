ba0d97fbbfc0f935d81b3aca6ca3c75945ff18d8
Gilson Junio Sales Andrade <gilsonjunio@quimicojapones.com.br>
2024-07-22 18:53:42 -0300

    comentários removidos

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-/*TODO
-SISTEMA DE SALVAR USUARIOS
-
-
-
-
-
-
-
-*/
-/*
-O SISTEMA DE SALVAR USUARIOS SÓ CONSEGUE SALVAR UM UNICO USUARIO 11/05/2024
-
-*/
-
 async function carregar_Dados(){
 	let buscar = await fetch('https://raw.githubusercontent.com/GilsonJunio/Alunos-Do-Lets-Code-2024/main/data.json');
 	console.log(buscar);
-
 	let objeto = await buscar.json();
 	console.log(objeto);
-
 	let dados = JSON.stringify(objeto.alunos);
-//	console.log(dados)
-
 	localStorage.setItem('usuarios', dados);
 
 }
@@ -32,35 +13,26 @@ carregar_Dados()
 let enviar = document.getElementById('enviar');
 enviar.addEventListener('click',() =>{
 
-	//SALVAR NOVOS USUÁRIOS	
 	let email = document.getElementById('email').value;
 		console.log(email);
-	
 	let senha = document.getElementById('senha').value;
 		console.log(senha);
-
 	let dados_coletar = {'dados':{'email_repassado':email,'senha_repassada':senha}};
 		console.log(dados_coletar);
 		console.log(dados_coletar.dados.email_repassado);
-
 	let dados_coletados = dados_coletar.dados;
 		console.log(dados_coletados);
 		console.log(dados_coletados.email_repassado);
-
 	let usuarios_novos = [];
 		usuarios_novos.push(dados_coletados)
 		console.log(usuarios_novos)
-	
-	localStorage.setItem('usuarios novos',JSON.stringify( usuarios_novos))
-	
-	// VERIFICAR SE EMAIL JÁ EXISTE
-
+	let procurar_email = buscar_usuarios.find((buscar_usuarios) => buscar_usuarios.email === email) || false;
+		console.log(procurar_email)	
 	let buscar_usuarios = JSON.parse(localStorage.getItem('usuarios'))
 		console.log(buscar_usuarios)
 		console.log(buscar_usuarios.email)
-
-	let procurar_email = buscar_usuarios.find((buscar_usuarios) => buscar_usuarios.email === email) || false;
-		console.log(procurar_email)
+	
+	localStorage.setItem('usuarios novos',JSON.stringify( usuarios_novos))
 
 	if(procurar_email === false){
 		alert("ESTE EMAIL NÃO FOI CADASTRADO")
@@ -69,13 +41,11 @@ enviar.addEventListener('click',() =>{
 		alert("ESTE EMAIL JÁ FOI CADASTRADO")
 	}
 
-//	let dados_comparar = dados_coletados.find((usuarios_novos) => dados_coletados.email_repassado === email && dados_coletados.senha_repassada === senha)
-//		console.log(dados_comparar)
-/*
 	if(dadosRepassados.email === ){
 
 	}
-*/
+
 	let usuarios = localStorage.getItem('usuarios')
 	
-	})
+	}
+)
