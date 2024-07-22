f7321b4b7cecd07bb01d1e6ddd9b8324095def2c
Gilson Junio Sales Andrade <gilsonjunio@quimicojapones.com.br>
2024-07-22 20:19:19 -0300

    Correções de sintaxe. Adicionados comentários em consoles para debug

--- script.js
+++ script.js
@@ -17,9 +17,11 @@ O SISTEMA DE SALVAR USUARIOS SÓ CONSEGUE SALVAR UM UNICO USUARIO 11/05/2024
 
 async function carregar_Dados(){
 	let buscar = await fetch('https://raw.githubusercontent.com/GilsonJunio/Alunos-Do-Lets-Code-2024/main/data.json');
+	console.log('-------RESULTADO DO FETCH-------');
 	console.log(buscar);
 
 	let objeto = await buscar.json();
+	console.log('-------OBJETO-------')
 	console.log(objeto);
 
 	let dados = JSON.stringify(objeto.alunos);
@@ -35,39 +37,55 @@ let enviar = document.getElementById('enviar');
 enviar.addEventListener('click',() =>{
 
 	//SALVAR NOVOS USUÁRIOS	
+	console.log('######### SALVAR NOVOS USUÁRIOS #########');
+
 	let email = document.getElementById('email').value;
+		console.log('--------EMAIL REPASSADO-----------');
 		console.log(email);
+
 	
 	let senha = document.getElementById('senha').value;
+		console.log('--------SENHA REPASSADA-----------');
 		console.log(senha);
 
 	let dados_coletar = {'dados':{'email_repassado':email,'senha_repassada':senha}};
+		console.log('--------COLETAR DADOS REPASSADOS-----------');
 		console.log(dados_coletar);
+		console.log('---------COLETAR DADOS REPASSADOS EMAIL----------');
 		console.log(dados_coletar.dados.email_repassado);
 
 	let dados_coletados = dados_coletar.dados;
+		console.log('-------DADOS COLETADOS------------');
 		console.log(dados_coletados);
+		console.log('---------DADOS COLETADOS EMAIL---------');
 		console.log(dados_coletados.email_repassado);
 
 	let usuarios_novos = [];
 		usuarios_novos.push(dados_coletados)
+		console.log('---------USUÁRIOS NOVOS----------');
 		console.log(usuarios_novos)
+		console.log(usuarios_novos[0].email_repassado)
 	
 	localStorage.setItem('usuarios novos',JSON.stringify( usuarios_novos))
 	
 	// VERIFICAR SE EMAIL JÁ EXISTE
-
+	console.log('######### VERIFICAR EMAIL EXISTENTE #########');
+	
 	let buscar_usuarios = JSON.parse(localStorage.getItem('usuarios'))
+		console.log('------------------BUSCAR USUARIOS----------------------');
 		console.log(buscar_usuarios)
 		console.log(buscar_usuarios.email)
 
 	let procurar_email = buscar_usuarios.find((buscar_usuarios) => buscar_usuarios.email === email) || false;
+		console.log('---------PROCURAR EMAIL NOS USUARIOS BUSCADOS----------');
 		console.log(procurar_email)
 
 	// VERIFICAR ESPAÇO VAZIO
+	console.log('######### VERIFICAR ESPAÇO VAZIO #########');
 
 	let procurar_vazio = dados_coletados.find((dados_coletados) => dados_coletados.email_repassado === email) || false;
-		console.log(procurar_email)
+		console.log('---------PROCURAR VAZIO----------');
+		console.log(procurar_vazio)
 
 	if(procurar_email === false){
 		alert("ESTE EMAIL NÃO FOI CADASTRADO")
